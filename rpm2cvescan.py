95f8c1f36a037d70ff539b4e9f5276d399bbf55b
Jan Huijsmans <huysmans@koffie.nu>
2019-08-28 15:16:54 +0200

    Rewrite python script
    Use another method to parse data. This way it's known what I get
    and how to get it.
    
    State of the dict entries checked with:
      python -m pdb ./rpm2cvescan.py
    
    CVSS Severity ratings from:
      https://nvd.nist.gov/vuln-metrics/cvss

--- rpm2cvescan.py
+++ rpm2cvescan.py
@@ -4,6 +4,8 @@
 # Monkey patch ElementTree
 import xml.etree.ElementTree as ET
 
+# Function to import xml in the order it's stored in the xml
+# instead of just random dump items in dict
 def _serialize_xml(write, elem, encoding, qnames, namespaces):
     tag = elem.tag
     text = elem.text
@@ -68,33 +70,90 @@ class OrderedXMLTreeBuilder(ET.XMLTreeBuilder):
 
 # =======================================================================
 
-tree = ET.parse('./com.redhat.rhsa-RHEL7.xml', OrderedXMLTreeBuilder())
+xml_tree = ET.parse('./com.redhat.rhsa-RHEL7.xml')
 
-root = tree.getroot()
-for child in root.iter():
-    #print(child.tag, child.attrib)
+#print xml_tree
 
-    # Get rhsa id and version
-    if child.tag.endswith('definition'):
-	print ""
-	print child.attrib['id'], " ", child.attrib['version']
+oval_definitions = xml_tree.getroot()
+for oval_subset in oval_definitions:
+    #print (oval_subset.tag, "->", oval_subset.__dict__)
+    print ""
 
-    # When CVE, get CVE ID
-    if child.tag.endswith('reference'):
-	if child.attrib['source'] == 'CVE':
-	    print child.attrib['ref_id']
+    if oval_subset.tag == '{http://oval.mitre.org/XMLSchema/oval-definitions-5}definitions':
+       for oval_definition in oval_subset:
 
-    # When ctiterion, get package name and version
-    if child.tag.endswith('criterion'):
-	if 'is earlier than' in child.attrib['comment']:
-	    comment = child.attrib['comment'].split()
-	    print comment[0], "<", comment[-1]
+	   #print oval_definition.tag, "-> ", oval_definition.__dict__
+	   patch_definition = oval_definition.attrib
 
-    # Get rhsa id and version
-    if child.tag.endswith('cve'):
-	if 'cvss2' in child.attrib.keys():
-	    print "cvss2:", child.attrib['cvss2'].split('/')[0], child.attrib['href'].split('/')[-1]
+	   print patch_definition['id'], " ", patch_definition['version']
 
-	if 'cvss3' in child.attrib.keys():
-	    print "cvss3:", child.attrib['cvss3'].split('/')[0], child.attrib['href'].split('/')[-1]
+	   for patch_data in oval_definition:
+	       #print patch_data.tag, "-> ", patch_data.__dict__
+	       # Get CVE ID
+	       if patch_data.tag == '{http://oval.mitre.org/XMLSchema/oval-definitions-5}metadata':
+		  for metadata_data in patch_data:
+		      if metadata_data.tag.endswith('reference'):
+			 #print ""
+			 #print metadata.__dict__
+			 #print ""
+			 if metadata_data.attrib['source'] == 'CVE':
+			    print "  CVE reference: ", metadata_data.attrib['ref_id']
 
+		  # get CVSS score of the CVE with this patch
+	          if metadata_data.tag == '{http://oval.mitre.org/XMLSchema/oval-definitions-5}advisory':
+		     for advisory_data in metadata_data:
+			 # Assumption: CVSS3 score takes president over CVSS2
+		         if 'cvss3' in advisory_data.keys():
+			    cve = advisory_data.attrib['href'].split('/')[-1]
+			    score_txt = advisory_data.attrib['cvss3'].split('/')[0]
+			    score = float(score_txt)
+			    severity = 'None'
+
+			    if score > 8.9:
+			       severity = 'Critical'
+			    elif score > 6.9:
+			       severity = 'High'
+			    elif score > 3.9:
+			       severity = 'Medium'
+			    elif score > 0:
+			       severity = 'Low'
+			       
+			    print " ", cve, "-" , score, "-", severity, "(cvss3)"
+		         elif 'cvss2' in advisory_data.keys():
+			    cve = advisory_data.attrib['href'].split('/')[-1]
+			    score_txt = advisory_data.attrib['cvss2'].split('/')[0]
+			    score = float(score_txt)
+			    severity = 'Low'
+
+			    if score > 6.9:
+			       severity = 'High'
+			    elif score > 3.9:
+			       severity = 'Medium'
+			       
+			    print " ", cve, "-" , score, "-", severity, "(cvss2)"
+
+	       # Weed trough criteria.
+	       # AT 1st level we alwais have criteria
+	       # at 2nd and later levels it is possible to find a criterion
+	       # with package name and version we need to store
+	       if patch_data.tag == '{http://oval.mitre.org/XMLSchema/oval-definitions-5}criteria':
+		  for criteria2 in patch_data:
+		      if criteria2.tag == '{http://oval.mitre.org/XMLSchema/oval-definitions-5}criteria':
+			 for criteria3 in criteria2:
+			     if criteria3.tag == '{http://oval.mitre.org/XMLSchema/oval-definitions-5}criteria':
+			        for criterion in criteria3:
+			            if criterion.tag == '{http://oval.mitre.org/XMLSchema/oval-definitions-5}criterion':
+				       if 'is earlier than' in criterion.attrib['comment']:
+				          comment = criterion.attrib['comment'].split()
+				          print "   ", comment[0], "<", comment[-1]
+			     elif criteria3.tag == '{http://oval.mitre.org/XMLSchema/oval-definitions-5}criterion':
+				if 'is earlier than' in criteria3.attrib['comment']:
+				   comment = criteria3.attrib['comment'].split()
+				   print "   ", comment[0], "<", comment[-1]
+		      elif criteria2.tag == '{http://oval.mitre.org/XMLSchema/oval-definitions-5}criterion':
+			 if 'is earlier than' in criteria2.attrib['comment']:
+			    comment = criteria2.attrib['comment'].split()
+			    print "   ", comment[0], "<", comment[-1]
+
+	   # Empty line to get a nice layout
+	   print ""
