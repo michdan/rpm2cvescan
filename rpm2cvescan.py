de90a8f4d3a50b0f262af7b50c482b71e00e181c
Jan Huijsmans <huysmans@koffie.nu>
2019-08-30 15:50:04 +0200

    Rewrite main routine to functions.
    Make sure we can handle all available versions.
    
    ToDo:
    * Split of epoch:version-release gives weird issues
      * From time to time, epoch drops away
      * el version gets cut off when it's 7_x (sometimes)
    * sometimes el is unavailable, need to find a solution for that

--- rpm2cvescan.py
+++ rpm2cvescan.py
@@ -2,6 +2,9 @@
 import rpm
 
 namespace='{http://oval.mitre.org/XMLSchema/oval-definitions-5}'
+patchlist = []
+rhelversions = ['RHEL5', 'RHEL6', 'RHEL7', 'RHEL8' ]
+base_patchfilename='./com.redhat.rhsa-'
 
 # =======================================================================
 # Monkey patch ElementTree
@@ -81,54 +84,59 @@ class OrderedXMLTreeBuilder(ET.XMLTreeBuilder):
 # =======================================================================
 class my_rpm:
     def __init__(self, name_string):
-        self.name = name_string.split('.')[0].rsplit('-',1)[0]
-	self.version_string = name_string.strip(self.name+'-')
-	if 'el' in self.version_string:
-	   self.rhversion = self.version_string.split('el')[1].split('.')[0]
-	else:
-	   self.rhversion = 7
+	if ':' in name_string:
+           self.name = name_string.split(':')[0].rsplit('-',1)[0]
+        else:
+           self.name = name_string.split('.')[0].rsplit('-',1)[0]
+
+        self.version_string = name_string.strip(self.name+'-')
+
+        if 'el' in self.version_string:
+           self.rhversion = self.version_string.split('el')[1].split('.')[0]
+        else:
+           self.rhversion = 7
 
     def __eq__(self, other):
         """Override the default Equals behavior"""
         if isinstance(other, self.__class__):
-	   if self.name == other.name:
-	      result = rpm.labelCompare((self.epoch(), self.version(), self.release()), (other.epoch(), other.version(), other.release()))
+           if self.name == other.name:
+              result = rpm.labelCompare((self.epoch(), self.version(), self.release()), (other.epoch(), other.version(), other.release()))
               if result == 0:
-	         return True
+                 return True
               else:
                  return False
            else:
               return False
         else:
-           return False
+           return NotImplemented
 
     def __ne__(self, other):
         """Override the default Not Equal behavior"""
         if isinstance(other, self.__class__):
-	   if self.name == other.name:
-	      result = rpm.labelCompare((self.epoch(), self.version(), self.release()), (other.epoch(), other.version(), other.release()))
+           if self.name == other.name:
+              result = rpm.labelCompare((self.epoch(), self.version(), self.release()), (other.epoch(), other.version(), other.release()))
               if result == 0:
-	         return False
+                 return False
               else:
                  return True
            else:
               return True
         else:
-           return True
+           return NotImplemented
 
     def __ge__(self, other):
         """Override the default Greater or equal behavior"""
         if isinstance(other, self.__class__):
-	   if self.name == other.name:
-	      result = rpm.labelCompare((self.epoch(), self.version(), self.release()), (other.epoch(), other.version(), other.release()))
+           if self.name == other.name:
+              result = rpm.labelCompare((self.epoch(), self.version(), self.release()), (other.epoch(), other.version(), other.release()))
               if result >= 0:
-	         return True
+                 return True
               else:
                  return False
            else:
               return False
         else:
-           return False
+           return NotImplemented
 
     def epoch(self):
         if ':' in self.version_string:
@@ -148,6 +156,13 @@ class my_rpm:
 
 # =======================================================================
 
+class my_patch:
+    def __init__(self, name, version):
+        self.name = name
+        self.version = int(version)
+        self.rhalist = []
+        self.cvelist = []
+        self.rpmlist = []
 
 # =======================================================================
 # Class my_cve
@@ -157,31 +172,31 @@ class my_rpm:
 class my_cve:
     def __init__(self, name, cvss, score):
         self.name = name
-	self.cvss = int(cvss)
-	self.score = float(score)
+        self.cvss = int(cvss)
+        self.score = float(score)
 
     #@classmethod
     def rating(self):
         if self.cvss == 3:
-	   if self.score > 8.9:
-	      severity = 'Critical'
-	   elif self.score > 6.9:
-	      severity = 'High'
-	   elif self.score > 3.9:
-	      severity = 'Medium'
-	   elif self.score > 0:
-	      severity = 'Low'
-	   else:
-	      severity = 'None'
-	elif self.cvss == 2:
-	   if self.score > 6.9:
-	      severity = 'High'
-	   elif self.score > 3.9:
-	      severity = 'Medium'
-	   else:
-	      severity = 'Low'
-	else:
-	   severity = 'Unknown'
+           if self.score > 8.9:
+              severity = 'Critical'
+           elif self.score > 6.9:
+              severity = 'High'
+           elif self.score > 3.9:
+              severity = 'Medium'
+           elif self.score > 0:
+              severity = 'Low'
+           else:
+              severity = 'None'
+        elif self.cvss == 2:
+           if self.score > 6.9:
+              severity = 'High'
+           elif self.score > 3.9:
+              severity = 'Medium'
+           else:
+              severity = 'Low'
+        else:
+           severity = 'Unknown'
 
         return severity
 
@@ -195,74 +210,113 @@ class my_cve:
 # of levels it uses for patch criteria
 # =======================================================================
 def recurse_criteria(my_criterion):
+    my_rpmlist = []
+
     if my_criterion.tag == namespace+'criteria':
        for my_criteria in my_criterion:
-	   recurse_criteria(my_criteria)
+           my_rpmlist += recurse_criteria(my_criteria)
     elif my_criterion.tag == namespace+'criterion':
        if 'is earlier than' in my_criterion.attrib['comment']:
           comment = my_criterion.attrib['comment'].split()
-	  RPM = my_rpm(comment[0]+'-'+comment[-1])
-	  print "   ", RPM.name, "<", RPM.version_string, "(", RPM.rhversion, ")"
+          rpmname_version = comment[0]+'-'+comment[-1]
+          my_rpmlist.append(my_rpm(rpmname_version))
+
+    return my_rpmlist
 
 # =======================================================================
 
-xml_tree = ET.parse('./com.redhat.rhsa-RHEL7.xml')
+# get_patchlist
+def get_patchlist(my_rhelversion):
+    
+    my_filename = base_patchfilename+my_rhelversion+'.xml'
+    xml_tree = ET.parse(my_filename)
 
-#print xml_tree
+    my_patchlist = []
+    #print xml_tree
 
-oval_definitions = xml_tree.getroot()
-for oval_subset in oval_definitions:
-    #print (oval_subset.tag, "->", oval_subset.__dict__)
-    print ""
+    oval_definitions = xml_tree.getroot()
+    for oval_subset in oval_definitions:
+        #print (oval_subset.tag, "->", oval_subset.__dict__)
 
-    if oval_subset.tag == namespace+'definitions':
-       for oval_definition in oval_subset:
+        if oval_subset.tag == namespace+'definitions':
+           for oval_definition in oval_subset:
 
-	   #print oval_definition.tag, "-> ", oval_definition.__dict__
-	   patch_definition = oval_definition.attrib
+               #print oval_definition.tag, "-> ", oval_definition.__dict__
+               patch_definition = oval_definition.attrib
 
-	   print patch_definition['id'], " ", patch_definition['version']
+               this_patch = my_patch(patch_definition['id'], patch_definition['version'])
 
-	   for patch_data in oval_definition:
-	       #print patch_data.tag, "-> ", patch_data.__dict__
-	       # Get CVE ID
-	       if patch_data.tag == namespace+'metadata':
-		  for metadata_data in patch_data:
-		      if metadata_data.tag.endswith('reference'):
-			 #print ""
-			 #print metadata.__dict__
-			 #print ""
-			 if metadata_data.attrib['source'] != 'CVE':
-			    print " ", metadata_data.attrib['ref_id']
+               for patch_data in oval_definition:
+                   #print patch_data.tag, "-> ", patch_data.__dict__
+                   # Get CVE ID
+                   if patch_data.tag == namespace+'metadata':
+                      for metadata_data in patch_data:
+                          if metadata_data.tag.endswith('reference'):
+                             #print ""
+                             #print metadata.__dict__
+                             #print ""
+                             if metadata_data.attrib['source'] != 'CVE':
+                                this_patch.rhalist.append(metadata_data.attrib['ref_id'])
 
-		  # get CVSS score of the CVE with this patch
-	          if metadata_data.tag == namespace+'advisory':
-		     for advisory_data in metadata_data:
-			 # Assumption: CVSS3 score takes president over CVSS2
-		         if 'cvss3' in advisory_data.keys():
-			    cve = advisory_data.attrib['href'].split('/')[-1]
-			    score_txt = advisory_data.attrib['cvss3'].split('/')[0]
-			    score = float(score_txt)
+                      # get CVSS score of the CVE with this patch
+                      if metadata_data.tag == namespace+'advisory':
+                         for advisory_data in metadata_data:
+                             # Assumption: CVSS3 score takes president over CVSS2
+                             if 'cvss3' in advisory_data.keys():
+                                cve = advisory_data.attrib['href'].split('/')[-1]
+                                score_txt = advisory_data.attrib['cvss3'].split('/')[0]
+                                score = float(score_txt)
 
-			    CVE = my_cve(cve, 3, score)
+                                this_patch.cvelist.append(my_cve(cve, 3, score))
 
-			    print " ", CVE.name, "-" , CVE.score, "-", CVE.rating(), "(cvss", CVE.cvss, ")"
-		         elif 'cvss2' in advisory_data.keys():
-			    cve = advisory_data.attrib['href'].split('/')[-1]
-			    score_txt = advisory_data.attrib['cvss2'].split('/')[0]
-			    score = float(score_txt)
+                             elif 'cvss2' in advisory_data.keys():
+                                cve = advisory_data.attrib['href'].split('/')[-1]
+                                score_txt = advisory_data.attrib['cvss2'].split('/')[0]
+                                score = float(score_txt)
 
-			    CVE = my_cve(cve, 2, score)
+                                this_patch.cvelist.append(my_cve(cve, 2, score))
 
-			    print " ", CVE.name, "-" , CVE.score, "-", CVE.rating(), "(cvss", CVE.cvss, ")"
+                   # Weed trough criteria.
+                   # At 1st level we always have criteria
+                   # At 2nd and later levels it is possible to find a criterion
+                   # with package name and version we need to store
+                   if patch_data.tag == namespace+'criteria':
+                      for criteria2 in patch_data:
+                          this_patch.rpmlist += recurse_criteria(criteria2)
 
-	       # Weed trough criteria.
-	       # At 1st level we always have criteria
-	       # At 2nd and later levels it is possible to find a criterion
-	       # with package name and version we need to store
-	       if patch_data.tag == namespace+'criteria':
-		  for criteria2 in patch_data:
-	   	      recurse_criteria(criteria2)
+               my_patchlist.append(this_patch)
+               del this_patch
 
-	   # Empty line to get a nice layout
-	   print ""
+    return my_patchlist
+
+# =======================================================================
+
+# get_patchlist
+def print_patchlist(my_patchlist):
+    
+    for patch in my_patchlist:
+        print patch.name, patch.version
+
+        for rha in patch.rhalist:
+            print '  {}'.format(rha)
+
+        for cve in patch.cvelist:
+            print ' ', cve.name, \
+                  '-', cve.score, \
+                  '-', cve.rating(), \
+                  '(cvss{})'.format(cve.cvss)
+
+        for rpm in patch.rpmlist:
+            print '   ', rpm.name, \
+                  '<', rpm.version_string, \
+                  '({})'.format(rpm.rhversion)
+
+        print ''
+# =======================================================================
+
+for rhelversion in rhelversions:
+    patchlist = get_patchlist(rhelversion)
+    print_patchlist(patchlist)
+
+# get rpm list via command:
+# rpm -qa --queryformat "%{NAME}-%{EPOCH}:%{VERSION}-%{RELEASE}\n" | sed 's/(none)/0/'
